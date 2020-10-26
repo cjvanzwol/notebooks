acf9ba4db9f11f61900d713e77a04987af1658ee
Cor Jan van Zwol <cjvanzwol@gmail.com>
2020-10-26 08:55:30 +0000

    backup for changing server

---
+++ Covid/data/install_service.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+
+echo "hello world"
+echo "this script will contain the installation of a service that checks and downloads new data on sett intervals" 
+echo "as you can see, this is a TO Do item"
